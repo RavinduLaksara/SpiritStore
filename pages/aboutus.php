d71f28247876a0a8dfa32ef354ac562fa8903d80
Prageethek <146775637+Prageethek@users.noreply.github.com>
2024-10-31 23:37:37 +0530

    create contact us page and add css

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -8,6 +8,7 @@ include("../Headers/customerHeader.html")
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="about-content">
---
+++ pages/contactus.php
@@ -0,0 +1,38 @@
+<?php
+include("../Headers\customerHeader.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="contact-content">
+        <h1>CONTACT US</h1>
+        <p>For any questions, comments or queries regarding the Wine World concierge service, please contact us at: <a href="">www.info@spiritstore.lk</a></p>
+        <p>&#x1F4CD;Pick Up Location:</p>
+        <p>  No 102 and 104,<br>
+            Kumaran Ratnam Road,<br>
+            Colombo 2 ​<br>
+            📞 Call Us on : 0777 004 600</p>
+        <p>Open 9 AM to 9 PM every day (Except Excise Holidays & Poya days)</p>
+
+<div class="location-container">
+<iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.2871937329305!2d79.91297847483604!3d6.9754031930253335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2597c8dde7e47%3A0x341e7e820c46d3ed!2sUniversity%20of%20Kelaniya!5e0!3m2!1sen!2slk!4v1730394720972!5m2!1sen!2slk" 
+    width="600" height="450" 
+    style="border:0;" 
+    allowfullscreen="" 
+    loading="lazy"
+ referrerpolicy="no-referrer-when-downgrade">
+</iframe>
+      
+</div>
+
+    </div>
+</body>
+
+</html>
