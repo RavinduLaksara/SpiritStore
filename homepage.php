cf8f3f6738ad230b7289589b1237cfa22abae432
sanduni4 <sanduniayeshika4@gmail.com>
2024-11-01 14:38:59 +0530

    homepage

---
+++ homepage.php
@@ -0,0 +1,69 @@
+<?php
+include("dbconnect.php");
+include("Headers/customerHeader.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link rel="stylesheet" href="style.css"  />
+
+</head>
+<body>
+
+<section class="hero">
+
+     <div class="slide">
+    <img src="image/home1 (3).jpg" alt="liquor bottle 3" class="animated-image">
+   
+    </div>
+
+    <div class="slide">
+    <img src="image/home1 (2).jpg" alt="liquor bottle 2" class="animated-image">
+    </div> 
+
+    <div class="slide">
+    <img src="image/home1 (1).jpg" alt="liquor bottle 1" class="animated-image">
+    </div>
+
+    <h1 class="hero-heading">Welcome to Spirit Store</h1>
+   
+    <p class="hero-subheading">Discover Our Exclusive Collection</p>
+
+</section>
+
+
+
+<section class="brandshow">
+<div class="brand-marquee">
+    <div class="marquee-content">
+        <img src="image/brand1.PNG" alt="Brand 1">
+        <img src="image/brand2.PNG" alt="Brand 2">
+        <img src="image/brand3.PNG" alt="Brand 3">
+        <img src="image/brand4.PNG" alt="Brand 4">
+        <img src="image/brand5.PNG" alt="Brand 5">
+        <img src="image/brand6.PNG" alt="Brand 6">
+        <img src="image/brand7.PNG" alt="Brand 7">
+        <img src="image/brand8.PNG" alt="Brand 8">
+        <img src="image/brand9.PNG" alt="Brand 9">
+        <img src="image/brand10.PNG" alt="Brand 10">
+
+        <img src="image/brand1.PNG" alt="Brand 1">
+        <img src="image/brand2.PNG" alt="Brand 2">
+        <img src="image/brand3.PNG" alt="Brand 3">
+        <img src="image/brand4.PNG" alt="Brand 4">
+        <img src="image/brand5.PNG" alt="Brand 5">
+        <img src="image/brand6.PNG" alt="Brand 6">
+        <img src="image/brand7.PNG" alt="Brand 7">
+        <img src="image/brand8.PNG" alt="Brand 8">
+        <img src="image/brand9.PNG" alt="Brand 9">
+        <img src="image/brand10.PNG" alt="Brand 10">
+        
+    </div>
+</div>
+</section>
+
+</body>
+</html>
