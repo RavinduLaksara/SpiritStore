d15e45b6d86cff8aa1337c8a249c245071b7e44f
sanduni4 <sanduniayeshika4@gmail.com>
2024-11-02 19:48:08 +0530

    add

--- pages/supplier-dashboard.php
+++ pages/supplier-dashboard.php
@@ -1,6 +1,5 @@
 <?php
-
-include('Headers\supplierHeadder.html');
+include('../Headers\supplierHeadder.html');
 ?>
 
 <!DOCTYPE html>
@@ -10,10 +9,17 @@ include('Headers\supplierHeadder.html');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Supplier Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
-    <h1>Supplier page</h1>
-</body>
 
+<section class="heros">
+    <img src="../image/dashboard.jpeg" class="animated-images">
+        <h1>SUPPLIER DASHBOARD</h1>
+    </section>
+
+</body>
 </html>
