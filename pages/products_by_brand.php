4b0ac31a8210f4165931ca3729171f954c629cca
Prageethek <146775637+Prageethek@users.noreply.github.com>
2024-11-07 23:13:41 +0530

    add new css files for style folder

--- pages/products_by_brand.php
+++ pages/products_by_brand.php
@@ -29,11 +29,15 @@ $result = $connection->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $category_name; ?></title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../styles\seller_products.css">
+    <link rel="stylesheet" href="../styles\products.css">
+
 </head>
 
 <body>
-    <div class="content">
-        <div class="title">
+    <div >
+        <div class='title'>
             <?php
             echo "
                     <h3>$brand_name</h3>
@@ -41,7 +45,7 @@ $result = $connection->query($sql);
                 ";
             ?>
         </div>
-        <div>
+        <div class='item-container'>
             <?php
             while ($product_row = $result->fetch_assoc()) {
                 // Get category Name
@@ -50,11 +54,11 @@ $result = $connection->query($sql);
                 $row = $result_category->fetch_array();
                 $category_name = $row[0];
                 echo "
-                        <div>
+                        <div class='item item-seller_products'>
                             <img src = '../{$product_row['photo']}' alt = 'Product image'>
                             <p>$category_name</p>
                             <p>{$product_row['name']}</p>
-                            <button><a href = '../pages/seller_products.php?id= $product_row[ProductID]''>View Sellers</a></button>
+                            <a class='card-button' href = '../pages/seller_products.php?id= $product_row[ProductID]''>View Sellers</a>
                         </div>
                     ";
             }
