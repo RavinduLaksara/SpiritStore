e26a90e65d574f344cfb180273f24a46efad03b4
sanduni4 <sanduniayeshika4@gmail.com>
2024-11-17 18:16:43 +0530

    c

--- Headers/supplierHeadder.php
+++ Headers/supplierHeadder.php
@@ -8,6 +8,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title></title>
 
+  <link rel="stylesheet" href="../styles/supplierh.css" />
+
   <script
     src="https://kit.fontawesome.com/a076d05399.js"
     crossorigin="anonymous"></script>
@@ -32,69 +34,10 @@
       <a href="#">Reports</a>
       <a href="<?= APP_URL ?>/homepage.php">Site Home</a>
 
-      <a href="#"><i class="fas fa-user"></i></a>
+      <a href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-user"></i></a>
 
     </nav>
-  </section><style>
-  
-  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap");
-
-* {
-margin: 0;
-padding: 0;
-box-sizing: border-box;
-font-family: "Roboto", sans-serif;
-}
-
-#header {
-display: flex;
-background: white;
-box-shadow: 0 5px 15px rgba(0, 0, 0, 0.06);
-z-index: 999;
-position: fixed;
-top: 0;
-left: 0;
-height: 100vh;
-width: 400px; /* Set the width for the vertical sidebar */
-padding: 20px;
-}
-
-.navbar {
-display: flex;
-flex-direction: column; /* Arrange links vertically */
-align-items: center;
-width: 100%; /* Full width */
-}
-
-.logo {
-
-margin-bottom: 20px;
-letter-spacing: 2px;
-margin-left: 100px;
-}
-
-.navbar a {
-text-decoration: none;
-padding: 10px 0;
-}
-
-.right-icons {
-margin-top: auto;
-display: flex;
-align-items: center;
-}
-
-.right-icons a {
-margin-left: 10px;
-color: #14151a;
-font-size: 20px;
-}
-
-.right-icons a:hover {
-color: rgb(223, 86, 51);
-}
-
-</style>
+  </section>
 </body>
 
 </html>
